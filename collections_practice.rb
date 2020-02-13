8c4f1ac65010864d7b602295c87acc728a95a19c
Cathrin Whitmer <catwhitmer@gmail.com>
2020-02-13 23:05:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,32 @@
+def sort_array_asc(num)
+  num.sort
+end
+
+def sort_array_desc(num)
+  num.sort.reverse
+end
+
+def sort_array_char_count(strings)
+  strings.sort { |a, b| a <=> b }
+end
+
+def swap_elements(elements)
+  
+end
+
+def reverse_array(num)
+  num.reverse
+end
+
+def find_a(letter)
+  
+end
+
+def sum_array(num)
+  
+end
+
+def add_s(word)
+  
+end
 
